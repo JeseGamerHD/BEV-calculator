2e84e3a35ee61203e98c39c8a1b2b427f1243906
JeseGamerHD <135455727+JeseGamerHD@users.noreply.github.com>
2025-02-11 13:00:47 +0200

    toggle switch basic validation

--- scripts/inputs/toggle-switch.js
+++ scripts/inputs/toggle-switch.js
@@ -27,3 +27,46 @@ toggleSwitches.forEach(toggleSwitch => {
     });
 });
 
+document.addEventListener("input", (inputEvent) => {
+    if(inputEvent.target.classList.contains("toggleInput-field")){
+        let inputField = inputEvent.target;
+        handleToggleInput(inputField);
+        console.log(inputField.dataset.value);
+    }
+});
+
+document.addEventListener("focusout", (focusoutEvent) => {
+    if(focusoutEvent.target.classList.contains("toggleInput-field")){
+        let inputField = focusoutEvent.target;
+
+        if(inputField.value === "") {
+            inputField.value = inputField.min;
+        } else {
+            inputField.value = parseFloat(inputField.value);
+        }
+    }
+})
+
+function handleToggleInput(inputField) {
+
+    // Allow empty, just don't update any value
+    // (User can backspace properly)
+    if(inputField.value === "") {
+        inputField.dataset.value = inputField.min;
+        return;
+    }
+
+    let newValue = Number(parseFloat(inputField.value));
+    if (newValue >= inputField.min && newValue <= inputField.max) {
+        inputField.dataset.value = newValue;
+    }
+    else if(newValue > inputField.max) {
+        inputField.value = inputField.max;
+        inputField.dataset.value = inputField.max;
+    }
+    else {
+        inputField.dataset.value = inputField.min;
+    }
+}
+
+
