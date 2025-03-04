216bc943765cfb040c68a0b6d9706bfb075cd62e
Maria-Pauliina Korhonen <maria.p.korhonen@gmail.com>
2025-03-04 12:27:06 +0200

    Created unit test for decimals in energy needed

--- tests/scripts/scriptsTest.js
+++ tests/scripts/scriptsTest.js
@@ -1,12 +1,16 @@
-import { calcEnergyNeededForRange } from '../../scripts/scripts.js';
+import { calcEnergyNeededForRange, calcChargeTimeForRange } from '../../scripts/scripts.js';
 
-describe('test suite: calculating energy needed for range', () => {
+describe('test suite: calculating energy to be charged', () => {
   it('calculates energy required with valid information', () => {
     expect(calcEnergyNeededForRange(200, 15, 20, 60, undefined)).toEqual(18);
   });
   
-  it('displays 0 when charging is not needed', () => {
+  it('returns 0 when charging is not needed', () => {
     expect(calcEnergyNeededForRange(100, 12, 100, 60, undefined)).toEqual(0);
+  })
+
+  it('calculates energy with values containing decimals', () => {
+    expect(calcEnergyNeededForRange(200, 13.7, 20, 57.5, undefined)).toEqual(15.9);
   })
 }
 );
